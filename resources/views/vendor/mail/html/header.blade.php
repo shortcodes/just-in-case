1be62c8c295092cc39549fb7de646271094c13b1
Roman Szymanski <r.szymanski@mindz.pl>
2025-11-09 13:44:40 +0100

    fix: use PNG logo instead of SVG in email templates for better email client compatibility

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ config('app.url') }}/logo.svg" class="logo" alt="{{ config('app.name') }}" style="height: 75px; width: auto; max-height: 75px;">
+<img src="{{ config('app.url') }}/logo.png" class="logo" alt="{{ config('app.name') }}" style="height: 75px; width: auto; max-height: 75px;">
 </a>
 </td>
 </tr>
